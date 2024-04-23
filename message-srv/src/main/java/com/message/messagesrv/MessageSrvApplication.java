4e6c973ff478ff4d2190ddf12083e9cf505c3517
tiejiaxiaobao <2922035952@qq.com>
2024-04-23 23:26:28 +0800

    Build a general framework

---
+++ message-srv/src/main/java/com/message/messagesrv/MessageSrvApplication.java
@@ -0,0 +1,13 @@
+package com.message.messagesrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSrvApplication.class, args);
+    }
+
+}
