3667780b7dab36c74c585bd8b05a5c5582bab527
东风 <zhangyang_2002@foxmail.com>
2024-04-25 19:22:20 +0800

    feature: 1. Organize modules 2. Solve the problem of not being able to introduce beans 3. Implement automatic configuration injection through the database

---
+++ message-common/src/main/java/com/message/common/domin/MessageTaskScheduleConfig.java
@@ -0,0 +1,37 @@
+package com.message.common.domin;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 调度配置信息
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "message_task_schedule_config")
+public class MessageTaskScheduleConfig {
+
+
+    /**
+     * 每次拉取任务数量
+     */
+    private int messageScheduleLimit;
+
+    /**
+     * 最大重试次数
+     */
+    private int maxRetryNum;
+
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+
+    /**
+     * 更新时间
+     */
+    private Long updateTime;
+}
