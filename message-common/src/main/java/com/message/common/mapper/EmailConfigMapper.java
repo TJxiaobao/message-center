357e30b28e11d7673949e1e4095c539ab0cff2a2
东风 <zhangyang_2002@foxmail.com>
2024-05-05 16:34:44 +0800

    feature:

--- message-common/src/main/java/com/message/common/mapper/EmailConfigMapper.java
+++ message-common/src/main/java/com/message/common/mapper/EmailConfigMapper.java
@@ -2,7 +2,6 @@ package com.message.common.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.message.common.domin.EmailConfig;
-import org.apache.ibatis.annotations.Mapper;
 
 
 public interface EmailConfigMapper extends BaseMapper<EmailConfig> {
